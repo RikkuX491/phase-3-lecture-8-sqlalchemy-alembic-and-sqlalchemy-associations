ca4c042628d8e3d4697411b937f57ffb52ef0c1b
Ricardo Guerra <RikkuG491@gmail.com>
2023-06-05 14:42:20 -0400

    Added solution code

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,54 +1,51 @@
-from sqlalchemy import ForeignKey, Table, Column, Integer, String
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 
-Base = declarative_base()
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
 
-# hotel_customer = Table(
-#     'hotel_customers',
-#     Base.metadata,
-#     Column('hotel_id', ForeignKey('hotels.id'), primary_key=True),
-#     Column('customer_id', ForeignKey('customers.id'), primary_key=True),
-#     extend_existing=True,
-# )
+m = MetaData(naming_convention=convention)
 
-# class Hotel(Base):
-#     __tablename__ = 'hotels'
+Base = declarative_base(metadata=m)
 
-#     id = Column(Integer(), primary_key=True)
-#     name = Column(String())
+class Hotel(Base):
+    __tablename__ = 'hotels'
 
-#     customers = relationship('Customer', secondary=hotel_customer, back_populates='hotels')
-#     # Later: Add 'secondary=hotel_customer' to the relationship
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
-#     reviews = relationship('Review', backref=backref('hotel'), cascade='all, delete-orphan')
+    reviews = relationship('Review', backref='hotel')
+    customers = association_proxy('reviews', 'customer',
+        creator=lambda c: Review(customer=c))
 
-#     def __repr__(self):
-#         return f'Hotel #{self.id}: {self.name}'
+    def __repr__(self):
+        return f'Hotel #{self.id}: {self.name}'
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    rating = Column(String())
+
+    hotel_id = Column(Integer(), ForeignKey('hotels.id'))
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+
+    def __repr__(self):
+        return f'Review #{self.id}: Rating is {self.rating}, left by {self.customer.first_name} {self.customer.last_name} for {self.hotel.name} hotel.'
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String())
+    last_name = Column(String())
+
+    reviews = relationship('Review', backref='customer')
+    hotels = association_proxy('reviews', 'hotel',
+        creator=lambda h: Review(hotel=h))
     
-# class Customer(Base):
-#     __tablename__ = 'customers'
-
-#     id = Column(Integer(), primary_key=True)
-#     first_name = Column(String())
-#     last_name = Column(String())
-
-#     hotels = relationship('Hotel', secondary=hotel_customer, back_populates='customers')
-#     # Later: Add 'secondary=hotel_customer' to the relationship
-
-#     reviews = relationship('Review', backref=backref('customer'), cascade='all, delete-orphan')
-
-#     def __repr__(self):
-#         return f'Customer #{self.id}: {self.first_name} {self.last_name}'
-
-# class Review(Base):
-#     __tablename__ = 'reviews'
-
-#     id = Column(Integer(), primary_key=True)
-#     rating = Column(Integer())
-
-#     hotel_id = Column(Integer(), ForeignKey('hotels.id'))
-#     customer_id = Column(Integer(), ForeignKey('customers.id'))
-
-#     def __repr__(self):
-#         return f'Review #{self.id}: Rating is {self.rating}'
+    def __repr__(self):
+        return f'Customer #{self.id}: {self.first_name} {self.last_name}'
--- lib/db/seed.py
+++ lib/db/seed.py
@@ -6,24 +6,23 @@ from models import Customer
 from models import Review
 
 if __name__ == '__main__':
-    pass
     
     engine = create_engine('sqlite:///hotels.db')
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # hotel1 = Hotel(name="Marriott")
-    # hotel2 = Hotel(name="Hampton Inn")
-    # hotel3 = Hotel(name="Holiday Inn")
+    hotel1 = Hotel(name="Marriott")
+    hotel2 = Hotel(name="Hampton Inn")
+    hotel3 = Hotel(name="Holiday Inn")
 
-    # customer1 = Customer(first_name="Alice", last_name="Baker")
-    # customer2 = Customer(first_name="Brendan", last_name="Johnson")
-    # customer3 = Customer(first_name="Chris", last_name="Smith")
+    customer1 = Customer(first_name="Alice", last_name="Baker")
+    customer2 = Customer(first_name="Brendan", last_name="Johnson")
+    customer3 = Customer(first_name="Chris", last_name="Smith")
 
-    # review1 = Review(rating=5, hotel_id=1, customer_id=1)
-    # review2 = Review(rating=5, hotel_id=2, customer_id=1)
-    # review3 = Review(rating=4, hotel_id=1, customer_id=2)
-    # review4 = Review(rating=3, hotel_id=1, customer_id=1)
+    review1 = Review(rating=5, hotel_id=1, customer_id=1)
+    review2 = Review(rating=5, hotel_id=2, customer_id=1)
+    review3 = Review(rating=4, hotel_id=1, customer_id=2)
+    review4 = Review(rating=3, hotel_id=1, customer_id=1)
 
-    # session.add_all([hotel1, hotel2, hotel3, customer1, customer2, customer3, review1, review2, review3, review4])
-    # session.commit()
+    session.add_all([hotel1, hotel2, hotel3, customer1, customer2, customer3, review1, review2, review3, review4])
+    session.commit()
